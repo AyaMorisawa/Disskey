2ec9d131c40722d522800c39a50a6b9245850665
Aya Morisawa <AyaMorisawa4869@gmail.com>
2015-08-23 02:04:12 +0900

    commit

---
+++ gulpfile.ts
@@ -0,0 +1,22 @@
+/// <reference path="typings/bundle.d.ts" />
+
+import { task, src, dest } from 'gulp';
+import * as ts from 'gulp-typescript';
+import * as tslint from 'gulp-tslint';
+
+var tsProject = ts.createProject('tsconfig.json');
+
+task('build', () => {
+	var tsResult = tsProject.src()
+		.pipe(ts(tsProject));
+
+	return tsResult.js.pipe(dest('./'));
+});
+
+task('tslint', () => {
+	return src('./src/**/*.ts')
+		.pipe(tslint())
+		.pipe(tslint.report('verbose'));
+});
+
+task('test', ['build', 'tslint']);
