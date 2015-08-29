d46a08a7a7e25006cac13dbb57da30d95966fb16
Aya Morisawa <AyaMorisawa4869@gmail.com>
2015-08-29 19:06:42 +0900

    commit

--- gulpfile.ts
+++ gulpfile.ts
@@ -3,20 +3,37 @@
 import { task, src, dest } from 'gulp';
 import * as ts from 'gulp-typescript';
 import * as tslint from 'gulp-tslint';
+import * as shelljs from 'shelljs';
+import * as rimraf from 'rimraf';
 
 var tsProject = ts.createProject('tsconfig.json');
 
-task('build', () => {
+task('start', () => {
+	shelljs.exec('electron .');
+});
+
+task('build', ['build:ts', 'build:html']);
+
+task('build:ts', () => {
 	var tsResult = tsProject.src()
 		.pipe(ts(tsProject));
 
-	return tsResult.js.pipe(dest('./'));
+	return tsResult.js.pipe(dest('./built'));
 });
 
-task('tslint', () => {
+task('build:html', () => {
+	return src('./src/**/*.html')
+		.pipe(dest('./built'));
+});
+
+task('lint', () => {
 	return src('./src/**/*.ts')
 		.pipe(tslint())
 		.pipe(tslint.report('verbose'));
 });
+
+task('clean', (cb) => {
+	rimraf('./built', cb);
+})
 
 task('test', ['build', 'tslint']);
