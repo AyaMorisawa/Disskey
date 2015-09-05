96b7a608a12946bd6c52f15a0c4949162d46b9df
Aya Morisawa <AyaMorisawa4869@gmail.com>
2015-09-05 15:02:44 +0900

    Fix #7

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,40 +1,46 @@
 import * as React from 'react';
 import { Token } from '../misskey';
+import IConfig from '../config/IConfig';
 import appConfig from '../config/app';
 import * as userConfigProvider from '../config/user';
 import AuthForm, { IAuthFormProps } from './AuthForm';
 import { Match } from 'satch';
+var objectAssign: (target: any, ...sources: any[]) => any = require('object-assign');
 
 var { div } = React.DOM;
 
 interface IAppState {
 	token?: Token;
-	checkingToken?: boolean;
+	ready?: boolean;
 	existToken?: boolean;
+	config?: IConfig;
 }
 
 class App extends React.Component<{}, IAppState> {
 	constructor(props?: {}, context?: any) {
 		super(props, context);
 		this.state = {
-			checkingToken: true,
+			ready: false,
 			existToken: false
 		};
+		(<any>window).app = this; // debug
 	}
 
 	componentDidMount() {
 		userConfigProvider.read().then(userConfig => {
-			var mergedConfig = userConfig; // TODO: merge(config, userConfig);
+			var mergedConfig: IConfig = objectAssign(appConfig, userConfig);
 			var existUserKey = mergedConfig.userKey !== void 0;
 			if (existUserKey) {
 				this.setState({
-					checkingToken: false,
-					token: new Token(appConfig.appKey, mergedConfig.userKey), // TODO: mergedConfig.appKey
-					existToken: true
+					ready: true,
+					token: new Token(mergedConfig.appKey, mergedConfig.userKey), // TODO: mergedConfig.appKey
+					existToken: true,
+					config: mergedConfig
 				});
 			} else {
 				this.setState({
-					checkingToken: false
+					ready: true,
+					config: mergedConfig
 				});
 			}
 		});
@@ -42,20 +48,20 @@ class App extends React.Component<{}, IAppState> {
 
 	onGetToken(token: Token) {
 		this.setState({
-			token, existToken: true
+			token,
+			existToken: true
 		});
 		userConfigProvider.read().then(userConfig => {
 			userConfig.userKey = token.userKey;
 			userConfigProvider.write(userConfig);
 		});
-		(<any>window).token = token; // debug
 	}
 
 	render() {
 		return new Match<any, React.DOMElement<React.HTMLAttributes> | React.ReactElement<IAuthFormProps>>(null)
-			.when(() => this.state.checkingToken, () => div({}, '[splash window]'))
+			.when(() => !this.state.ready, () => div({}, '[splash window]'))
 			.when(() => !this.state.existToken, () => React.createElement<IAuthFormProps>(AuthForm, {
-				appKey: appConfig.appKey, // TODO: merge
+				appKey: this.state.config.appKey, // TODO: merge
 				onGetToken: this.onGetToken.bind(this)
 			}))
 			.default(() => div({}, `Your user-key: ${this.state.token.userKey}`));
