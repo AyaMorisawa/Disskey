ad4ab5ed32f9f5c374cd549519f3097750df6414
Aya Morisawa <AyaMorisawa4869@gmail.com>
2015-09-22 14:07:43 +0900

    Use gulp-plumber and gulp-notify
    Close #30

--- gulpfile.ts
+++ gulpfile.ts
@@ -5,6 +5,8 @@ import * as ts from 'gulp-typescript';
 import * as tslint from 'gulp-tslint';
 import * as shelljs from 'shelljs';
 import * as del from 'del';
+import * as plumber from 'gulp-plumber';
+var notify = require('gulp-notify');
 var babel = require('gulp-babel');
 
 var tsProject = ts.createProject('tsconfig.json', <any>{
@@ -25,6 +27,7 @@ task('build', ['build:ts', 'build:html', 'build:css']);
 
 task('build:ts', () => {
 	return tsProject.src()
+		.pipe(plumber({errorHandler: notify.onError('Build error: <%= error.message %>')}))
 		.pipe(ts(tsProject))
 		.pipe(babel({
 			modules: 'commonStrict'
@@ -45,6 +48,7 @@ task('build:css', () => {
 
 task('lint', () => {
 	return src('./src/**/*.ts')
+		.pipe(plumber({errorHandler: notify.onError('Lint error: <%= error.message %>')}))
 		.pipe(tslint())
 		.pipe(tslint.report('verbose'));
 });
