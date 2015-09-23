2661e0bd20c53f50931640c14698e1edb30f68f1
Aya Morisawa <AyaMorisawa4869@gmail.com>
2015-09-23 16:28:53 +0900

    Create command.ts

---
+++ src/command.ts
@@ -0,0 +1,7 @@
+import * as shelljs from 'shelljs';
+import * as path from 'path';
+import * as fs from 'fs';
+
+var packageDir = path.join(path.dirname(fs.realpathSync(__filename)), '../');
+shelljs.cd(packageDir);
+shelljs.exec(path.join(packageDir, 'node_modules', '.bin', 'electron') + ' .');
