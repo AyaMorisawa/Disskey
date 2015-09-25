b34117ec84770d93c1ec2ec5a5e3f58eec502ed9
Aya Morisawa <AyaMorisawa4869@gmail.com>
2015-09-25 19:27:26 +0900

    Use let instead of var

--- src/command.ts
+++ src/command.ts
@@ -2,6 +2,6 @@ import * as shelljs from 'shelljs';
 import * as path from 'path';
 import * as fs from 'fs';
 
-var packageDir = path.join(path.dirname(fs.realpathSync(__filename)), '../');
+let packageDir = path.join(path.dirname(fs.realpathSync(__filename)), '../');
 shelljs.cd(packageDir);
 shelljs.exec(path.join(packageDir, 'node_modules', '.bin', 'electron') + ' .');
