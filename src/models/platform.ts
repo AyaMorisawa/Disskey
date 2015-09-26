7c5a17a916d1f52612f695d226a217710bb494da
Aya Morisawa <AyaMorisawa4869@gmail.com>
2015-09-26 20:22:07 +0900

    Use const instead of let

--- src/models/platform.ts
+++ src/models/platform.ts
@@ -1,6 +1,6 @@
-let isLinux = process.platform === 'linux';
-let isFreeBSD = process.platform === 'freebsd';
-let isOSX = process.platform === 'darwin';
-let isSunOS = process.platform === 'sunos';
+const isLinux = process.platform === 'linux';
+const isFreeBSD = process.platform === 'freebsd';
+const isOSX = process.platform === 'darwin';
+const isSunOS = process.platform === 'sunos';
 
 export { isLinux, isFreeBSD, isOSX, isSunOS }
