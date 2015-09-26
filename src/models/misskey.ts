589893a5429546db33f70fb555244211e55e5d17
Aya Morisawa <AyaMorisawa4869@gmail.com>
2015-09-26 19:19:51 +0900

    createTimelineStream -> timelineStream

--- src/models/misskey.ts
+++ src/models/misskey.ts
@@ -93,40 +93,38 @@ export class MisskeyApi {
 }
 
 export class StatusApi extends MisskeyApi {
-	createTimelineStream(): any {
-		return Kefir.stream((emitter: any) => {
-			let isActive = true;
-			let lastCursor: number = void 0;
-			let f = () => {
-				this.getTimeline({sinceCursor: lastCursor})
-					.then(statuses => statuses.sort((a, b) => a.cursor - b.cursor))
-					.then(statuses => {
-						if (statuses.length >= 1) {
-							statuses.filter(isNewStatus).forEach(emitter.emit);
-							let lastStatus = statuses[statuses.length - 1];
-							if (isNewStatus(lastStatus)) {
-								lastCursor = lastStatus.cursor;
-							}
+	timelineStream = Kefir.stream((emitter: any) => {
+		let isActive = true;
+		let lastCursor: number = void 0;
+		let f = () => {
+			this.getTimeline({sinceCursor: lastCursor})
+				.then(statuses => statuses.sort((a, b) => a.cursor - b.cursor))
+				.then(statuses => {
+					if (statuses.length >= 1) {
+						statuses.filter(isNewStatus).forEach(emitter.emit);
+						let lastStatus = statuses[statuses.length - 1];
+						if (isNewStatus(lastStatus)) {
+							lastCursor = lastStatus.cursor;
 						}
-						function isNewStatus(status: any) {
-							return lastCursor === void 0 || status.cursor > lastCursor;
-						}
-					})
-					.then(next, next);
-				function next() {
-					let interval = 1000;
-					if (isActive) {
-						setTimeout(f, interval);
 					}
+					function isNewStatus(status: any) {
+						return lastCursor === void 0 || status.cursor > lastCursor;
+					}
+				})
+				.then(next, next);
+			function next() {
+				let interval = 1000;
+				if (isActive) {
+					setTimeout(f, interval);
 				}
-			};
-			f();
-			function deactive() {
-				isActive = false;
 			}
-			return deactive;
-		});
-	}
+		};
+		f();
+		function deactive() {
+			isActive = false;
+		}
+		return deactive;
+	});
 
 	getTimeline(options: {sinceCursor?: number, maxCursor?: number, count?: number} = {}) {
 		return this.token.callApiWithHeaders<any[]>('status/timeline', {
