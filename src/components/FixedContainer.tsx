3ee939d128ab885c9c3773dba2fb659c1fff16b8
Aya Morisawa <AyaMorisawa4869@gmail.com>
2015-09-26 20:20:22 +0900

    Not bind style

--- src/components/FixedContainer.tsx
+++ src/components/FixedContainer.tsx
@@ -2,16 +2,14 @@ import * as React from 'react';
 
 export default class FixedContainer extends React.Component<{children?: JSX.Element, style?: any}, {}> {
 	render() {
-		const style = Object.assign({
-			position: 'absolute',
-			top: 0,
-			right: 0,
-			bottom: 0,
-			left: 0
-		}, this.props.style);
-
 		return (
-			<div style={style}>{this.props.children}</div>
+			<div style={Object.assign({
+				position: 'absolute',
+				top: 0,
+				right: 0,
+				bottom: 0,
+				left: 0
+			}, this.props.style)}>{this.props.children}</div>
 		);
 	}
 }
