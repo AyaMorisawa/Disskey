408cf6185ab8f81197f2d404c10fb1afa5caa87c
Aya Morisawa <AyaMorisawa4869@gmail.com>
2015-10-02 14:52:18 +0900

    Use if statement instead of conditional operator

--- src/models/request-promise.ts
+++ src/models/request-promise.ts
@@ -4,7 +4,11 @@ function requestPromise(options: request.Options) {
 	'use strict';
 	return new Promise<string>((resolve, reject) => {
 		request(options, (error, response, body) => {
-			error ? reject(error) : resolve(body);
+			if (error) {
+				reject(error);
+			} else {
+				resolve(body);
+			}
 		});
 	});
 }
